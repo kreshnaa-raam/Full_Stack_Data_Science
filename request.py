afabeeebafc0e42000c3ba3050255516fe04cbda
KreshnaaRaam <52041687+kreshnaa-raam@users.noreply.github.com>
2021-04-02 12:42:36 +0530

    Add files via upload

---
+++ request.py
@@ -0,0 +1,10 @@
+import requests
+
+url = 'http://139.59.36.35:80/predict_api'
+r = requests.post(url, json={'FlightDate': ["2010-01-16"],
+                             'UniqueCarrier': ['9E'],
+                             'Origin': ["ATL"],
+                             'Dest': ["AUS"],
+                             'Distance': [356]})
+
+print(r.json())
